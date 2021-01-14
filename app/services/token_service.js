a9b6cbf38980752619152d9b4c50ce33ba8efd88
Ryan Goodwin <minedied@gmail.com>
2021-01-14 16:54:22 -0700

    harumph

--- app/services/token_service.js
+++ app/services/token_service.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const config = require('config');
 const realmKey = config.get('RealmKey');
-const secret = config.get('SECRET');
-const publicKey = config.get('PUBLIC');
-const privateKey = config.get('PRIVATE');
+const secret = process.env.SECRET;
+const publicKey = process.env.PUBLIC;
+const privateKey = process.env.PRIVATE;
 
 exports.asynchToken = (payload) => {
   console.log("token call asynch ", secret)
