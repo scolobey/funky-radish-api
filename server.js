4d9fd9b55b881f759520cde1d0d2744221dabb9c
Ryan Goodwin <minedied@gmail.com>
2021-01-17 11:31:27 -0700

    cors setup

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ const app = express();
 
 app.use(cors());
 
- // Todo: set this up once gone live.
-// var whitelist = ['https://funkyradish.com', 'http://localhost:3000']
-//
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-//
+ // Todo: Set up dev and prod here so we can do localhost.
+var whitelist = ['https://funkyradish.com']
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 // // Then pass them to cors:
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
