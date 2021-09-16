43beb916627711fe03a58f5516c5939b92688008
Ryan Goodwin <minedied@gmail.com>
2021-09-15 20:02:41 -0600

    catching up a bit

--- app/services/token_service.js
+++ app/services/token_service.js
@@ -3,34 +3,29 @@ const fs = require('fs');
 const config = require('config');
 const realmKey = config.get('RealmKey');
 
+
+//TODO: This is an issue when the process is not set.
 const secret = process.env.SECRET || config.get('SECRET');
-// const publicKey = process.env.PUBLIC || config.get('PUBLIC');
-// const privateKey = process.env.PRIVATE || config.get('PRIVATE');
+const publicKey = process.env.PUBLIC || config.get('PUBLIC');
+const privateKey = process.env.PRIVATE || config.get('PRIVATE');
 
-
-const publicKey = fs.readFileSync("publickey.pem");
-const privateKey = fs.readFileSync("privatekey.pem");
+// Swap comments if you want to test keys output by
+// const publicKey = fs.readFileSync("publickey.pem");
+// const privateKey = fs.readFileSync("privatekey.pem");
 
 exports.asynchToken = (payload) => {
-  console.log("token call asynch ", secret)
   let currentTime = Date.now();
   let expTime = currentTime + 86400
 
   payload.sub = payload.user
   payload.aud = realmKey
 
-
   let tokenPromise = new Promise(function(resolve, reject) {
-    console.log("signing token")
-    console.log("key: " + privateKey)
-    console.log("phrase: " + secret)
     const token = jwt.sign(payload, { key: privateKey, passphrase: secret }, { algorithm: 'RS256', expiresIn: 86400 }, function(err, token) {
       if (err) {
-        console.log("rejected")
         reject(err);
       }
       else {
-        console.log("resolved")
         resolve(token)
       }
     });
@@ -55,21 +50,13 @@ exports.generateToken = (payload) => {
 }
 
 exports.verifyToken = (token) => {
-
-  console.log("verifying token.")
-  console.log("pubKey: " + publicKey)
-  console.log("token: " + token)
-
   let tokenVerificationPromise = new Promise(function(resolve, reject) {
-
     jwt.verify(token, publicKey, { format: 'PKCS8', algorithms: ['RS256'], ignoreExpiration: true }, function(err, decoded) {
-      console.log("decoded: " + decoded)
       if (err) {
-        console.log("verification error")
-        console.log(err)
         reject(err)
       }
       else {
+        console.log("verified?")
         resolve(decoded)
       }
     });
