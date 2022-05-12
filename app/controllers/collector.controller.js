759cded5d98a61739ec704912ffe596c1d9c5b51
Ryan Goodwin <minedied@gmail.com>
2022-05-12 16:20:28 -0600

    added timeout on recipe inspector

--- app/controllers/collector.controller.js
+++ app/controllers/collector.controller.js
@@ -63,70 +63,75 @@ exports.inspectRecipe = (req, res) => {
   let url = req.params.url
   console.log("crawling: " + url)
 
-  axios(url)
-    .then(response => {
+  axios({
+    method: 'get',
+    url: url,
+    timeout: 3000
+  })
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-      const html = response.data;
-      const $ = cheerio.load(html);
+    let jsonld;
 
-      let jsonld;
+    const node = $('script[type="application/ld+json"]').get(0);
 
-      const node = $('script[type="application/ld+json"]').get(0);
+    try {
+      jsonld = JSON.parse(node.firstChild.data)
 
-      try {
-        jsonld = JSON.parse(node.firstChild.data)
+      // jsonld presents as an array of jsonld objects
+      if (Array.isArray(jsonld)) {
+        let filteredList = jsonld.filter(function(schemaFilter) {
+          return schemaFilter["@type"] == "Recipe";
+        })
 
-        // jsonld presents as an array of jsonld objects
-        if (Array.isArray(jsonld)) {
-          let filteredList = jsonld.filter(function(schemaFilter) {
-            return schemaFilter["@type"] == "Recipe";
-          })
-
-          jsonld = filteredList.pop()
-        }
-
-        // Simplest format. Top level recipe
-        if (jsonld && jsonld.recipeIngredient && jsonld.recipeInstructions && jsonld.name) {
-
-          // Rare issue. Instruction array is wrapped in an extra array.
-          // https%3A%2F%2Ffood52.com%2Frecipes%2F87220-sambal-potatoes-aioli-recipe
-          if (!jsonld.recipeInstructions[0].text) {
-            jsonld.recipeInstructions = jsonld.recipeInstructions[0]
-          }
-
-          res.json({
-            ingredients: jsonld.recipeIngredient.map((ing) => ing.replace(/<\/?[^>]+(>|$)/g, "")),
-            directions: jsonld.recipeInstructions.map((dir) => dir.text.replace(/<\/?[^>]+(>|$)/g, "")),
-            title: jsonld.name
-          });
-        }
-        // Recipe is nested within @graph type
-        else if (jsonld["@graph"]){
-          let rec = jsonld["@graph"].filter(function(schemaFilter) {
-            return schemaFilter["@type"] == "Recipe";
-          })
-
-          if (rec.length > 0) {
-
-            res.json({
-              ingredients: rec[0].recipeIngredient.map((ing) => ing.replace(/<\/?[^>]+(>|$)/g, "")),
-              directions: rec[0].recipeInstructions.map((dir) => dir.text.replace(/<\/?[^>]+(>|$)/g, "")),
-              title: rec[0].name
-            });
-          } else {
-            res.status(500).send({ message: "Can't find recipe markup." });
-          }
-        }
-        else {
-          console.log("jsonld: " + JSON.stringify(jsonld))
-          res.status(500).send({ message: "Recipe markup missing" });
-        }
-      } catch (err) {
-        console.log(err)
-        // In case of error, you can try to debug by logging the node
-        res.status(500).send({ message: "Can't find recipe markup." });
+        jsonld = filteredList.pop()
       }
 
-    })
-    .catch(console.error);
+      // Simplest format. Top level recipe
+      if (jsonld && jsonld.recipeIngredient && jsonld.recipeInstructions && jsonld.name) {
+
+        // Rare issue. Instruction array is wrapped in an extra array.
+        // https%3A%2F%2Ffood52.com%2Frecipes%2F87220-sambal-potatoes-aioli-recipe
+        if (!jsonld.recipeInstructions[0].text) {
+          jsonld.recipeInstructions = jsonld.recipeInstructions[0]
+        }
+
+        res.json({
+          ingredients: jsonld.recipeIngredient.map((ing) => ing.replace(/<\/?[^>]+(>|$)/g, "")),
+          directions: jsonld.recipeInstructions.map((dir) => dir.text.replace(/<\/?[^>]+(>|$)/g, "")),
+          title: jsonld.name
+        });
+      }
+      // Recipe is nested within @graph type
+      else if (jsonld["@graph"]){
+        let rec = jsonld["@graph"].filter(function(schemaFilter) {
+          return schemaFilter["@type"] == "Recipe";
+        })
+
+        if (rec.length > 0) {
+
+          res.json({
+            ingredients: rec[0].recipeIngredient.map((ing) => ing.replace(/<\/?[^>]+(>|$)/g, "")),
+            directions: rec[0].recipeInstructions.map((dir) => dir.text.replace(/<\/?[^>]+(>|$)/g, "")),
+            title: rec[0].name
+          });
+        } else {
+          res.status(500).send({ message: "Can't find recipe markup." });
+        }
+      }
+      else {
+        console.log("jsonld: " + JSON.stringify(jsonld))
+        res.status(500).send({ message: "Recipe markup missing" });
+      }
+    } catch (err) {
+      console.log(err)
+      // In case of error, you can try to debug by logging the node
+      res.status(500).send({ message: "Can't find recipe markup." });
+    }
+  })
+  .catch((error) => {
+    res.status(500).send({ message: "Can't find recipe page: " + error });
+  })
+
 };
