f879e0d594c767d28997881aea7e64e717f8fee1
Ryan Goodwin <minedied@gmail.com>
2022-05-12 18:24:08 -0600

    improved the recipe inspector

--- app/controllers/collector.controller.js
+++ app/controllers/collector.controller.js
@@ -74,13 +74,22 @@ exports.inspectRecipe = (req, res) => {
 
     let jsonld;
 
-    const node = $('script[type="application/ld+json"]').get(0);
+    let nodesList = $('script[type="application/ld+json"]')
 
-    try {
+    var i = 0;
+
+    while (i < nodesList.length) {
+      console.log("checking: " + i);
+
+      const node = nodesList[i]
+
+      //parse the firstChild
       jsonld = JSON.parse(node.firstChild.data)
 
-      // jsonld presents as an array of jsonld objects
+      // If it's an array of jsonld objects
       if (Array.isArray(jsonld)) {
+        console.log("array: " + jsonld);
+
         let filteredList = jsonld.filter(function(schemaFilter) {
           return schemaFilter["@type"] == "Recipe";
         })
@@ -88,50 +97,69 @@ exports.inspectRecipe = (req, res) => {
         jsonld = filteredList.pop()
       }
 
-      // Simplest format. Top level recipe
+      // If the recipe is at the top.
       if (jsonld && jsonld.recipeIngredient && jsonld.recipeInstructions && jsonld.name) {
 
-        // Rare issue. Instruction array is wrapped in an extra array.
-        // https%3A%2F%2Ffood52.com%2Frecipes%2F87220-sambal-potatoes-aioli-recipe
+        console.log("Top level recipe");
+
+        let dirStringArray = []
+
         if (!jsonld.recipeInstructions[0].text) {
-          jsonld.recipeInstructions = jsonld.recipeInstructions[0]
+          console.log("instruction string array")
+          if (typeof jsonld.recipeInstructions[0] === 'string') {
+            console.log("instruction string array")
+            // just a regular string array
+            // https%3A%2F%2Fwww.midcenturymenu.com%2Frecipe%2Fcranberry-candle-salad%2F
+            dirStringArray = jsonld.recipeInstructions.map((dir) => dir.replace(/<\/?[^>]+(>|$)/g, ""))
+          } else {
+            console.log("instruction array within array")
+            // Instruction array is wrapped in an extra array.
+            // https%3A%2F%2Ffood52.com%2Frecipes%2F87220-sambal-potatoes-aioli-recipe
+            dirStringArray = jsonld.recipeInstructions[0].map((dir) => dir.text.replace(/<\/?[^>]+(>|$)/g, ""))
+          }
+        } else {
+          console.log("instruction object array")
+          dirStringArray = jsonld.recipeInstructions.map((dir) => dir.text.replace(/<\/?[^>]+(>|$)/g, ""))
         }
 
-        res.json({
+        return res.json({
           ingredients: jsonld.recipeIngredient.map((ing) => ing.replace(/<\/?[^>]+(>|$)/g, "")),
-          directions: jsonld.recipeInstructions.map((dir) => dir.text.replace(/<\/?[^>]+(>|$)/g, "")),
+          directions: dirStringArray,
           title: jsonld.name
         });
-      }
-      // Recipe is nested within @graph type
-      else if (jsonld["@graph"]){
-        let rec = jsonld["@graph"].filter(function(schemaFilter) {
-          return schemaFilter["@type"] == "Recipe";
-        })
+      } else {
 
-        if (rec.length > 0) {
+        // Recipe is nested within @graph type
+        if (jsonld["@graph"]){
+          console.log("checking in graph");
 
-          res.json({
-            ingredients: rec[0].recipeIngredient.map((ing) => ing.replace(/<\/?[^>]+(>|$)/g, "")),
-            directions: rec[0].recipeInstructions.map((dir) => dir.text.replace(/<\/?[^>]+(>|$)/g, "")),
-            title: rec[0].name
-          });
-        } else {
-          res.status(500).send({ message: "Can't find recipe markup." });
+          let rec = jsonld["@graph"].filter(function(schemaFilter) {
+            return schemaFilter["@type"] == "Recipe";
+          })
+
+          if (rec.length > 0) {
+            console.log("recipe detected in graph: " + JSON.stringify(rec));
+            return res.json({
+              ingredients: rec[0].recipeIngredient.map((ing) => ing.replace(/<\/?[^>]+(>|$)/g, "")),
+              directions: rec[0].recipeInstructions.map((dir) => dir.text.replace(/<\/?[^>]+(>|$)/g, "")),
+              title: rec[0].name
+            });
+          }
         }
+
       }
-      else {
-        console.log("jsonld: " + JSON.stringify(jsonld))
-        res.status(500).send({ message: "Recipe markup missing" });
+
+      if (i == nodesList.length-1) {
+        console.log("over");
+        return res.status(500).send({ message: "Couldn't find anything" });
       }
-    } catch (err) {
-      console.log(err)
-      // In case of error, you can try to debug by logging the node
-      res.status(500).send({ message: "Can't find recipe markup." });
+
+      i++
     }
+
   })
   .catch((error) => {
-    res.status(500).send({ message: "Can't find recipe page: " + error });
+    return res.status(500).send({ message: "Can't find recipe page: " + error });
   })
 
 };
