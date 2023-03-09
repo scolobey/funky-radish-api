8b730f81f15507cc3a9cd0f950d4abae380092a8
Ryan Goodwin <minedied@gmail.com>
2023-03-08 22:50:30 -0700

    added recipes to ingredient api
    after pulling ingredients from supabase, I do a quick call to the db.

--- app/controllers/ingredient.controller.js
+++ app/controllers/ingredient.controller.js
@@ -1,6 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 const config = require('config');
 
+const assert = require('assert');
+
 const supabaseUrl = process.env.SUPABASE_URI || config.get('SUPABASE_URI')
 const supabaseKey = process.env.SUPABASE_KEY || config.get('SUPABASE_KEY')
 
@@ -10,18 +12,55 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 exports.search = async (req, res) => {
   console.log("getting ingredient: " + req.params.query);
 
+  const database = req.app.locals.db
+
   let query = req.params.query
   let data = await getIngredients(query)
 
-  res.send(JSON.stringify(data))
+  console.log(data[0].recipes.length);
+
+  // collect the recipes.
+  let recipeReturn = getRecipes(database, data[0].recipes)
+
+  recipeReturn.toArray(function(err, docs) {
+    assert.equal(err, null);
+
+    console.log("recipes collected")
+
+    docs.forEach((item, i) => {
+      item.directions = item.direction_list
+      item.ingredients = item.ingredient_list
+
+      delete item.direction_list
+      delete item.ingredient_list
+    });
+
+    console.log("got back the recipe list: ");
+
+    data[0].recipes = docs
+
+    res.send(JSON.stringify(data))
+  })
+
 }
 
 const getIngredients = async (ingName) => {
-
   const { data, error } = await supabase
     .from('ingredients')
     .select()
     .eq('name', ingName)
+    .ilike('name', '%' + ingName + '%')
 
   return data
+};
+
+const getRecipes = (db, ingredients) => {
+  // let page = req.params.page || 1
+  let mongoQuery ={_id: { $in: ingredients }}
+
+  return db.collection('Recipe')
+  .find(mongoQuery)
+  // .skip((page-1)*30)
+  .limit(30)
+
 };
